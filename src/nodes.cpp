e886f72f867e5816af8bf255d2f90a4df0f32d30
KFerdek <kferdek@student.agh.edu.pl>
2026-01-22 17:46:37 +0100

    Documentation
    Comments and comments

--- src/nodes.cpp
+++ src/nodes.cpp
@@ -6,13 +6,20 @@
 #include "nodes.hpp"
 
 
-
+//implementation of probability generator function
+//Needs to be moved to other file!!!
 std::random_device rd;
 std::mt19937 rng(rd());
 double probability_generator() {
   return std::generate_canonical<double, 10>(rng);
 }
+
 void ReceiverPreferences::add_receiver(IPackageReceiver* receiver) {
+  /*method that adds the receiver to the receiver pool
+   and updates the map values accordingly
+   takes a pointer to IPackageReceiver
+   returns nothing
+   */
   prefs.insert(std::pair{receiver,1});
   if (prefs.size()>1) {
     double val = 1.0/static_cast<double>(prefs.size());
@@ -22,6 +29,11 @@ void ReceiverPreferences::add_receiver(IPackageReceiver* receiver) {
   }
 }
 void ReceiverPreferences::remove_receiver(IPackageReceiver* receiver) {
+  /*method that removes the receiver from the receiver pool
+ and updates the map values accordingly
+ takes a pointer to IPackageReceiver
+ returns nothing
+ */
   prefs.erase(receiver);
   if (not prefs.empty()) {
     double val = 1.0/static_cast<double>(prefs.size());
@@ -31,6 +43,11 @@ void ReceiverPreferences::remove_receiver(IPackageReceiver* receiver) {
   }
 }
 IPackageReceiver* ReceiverPreferences::choose_receiver() {
+  /*method that chooses the receiver from the receiver pool
+   based on the receiver pool and probability_generator
+   takes nothing
+   returns a pointer to IPackageReceiver
+ */
   double rand = pg();
   double threshold = prefs.begin()->second;
   int index = floor(rand/threshold);
@@ -38,15 +55,31 @@ IPackageReceiver* ReceiverPreferences::choose_receiver() {
 }
 
 void PackageSender::push_package(Package&& package) {
+  /*
+   protected method that adds a package into a buffer
+   takes Package r-value
+   returns nothing
+   */
   buffer = std::move(package);
 }
 
 void PackageSender::send_package() {
+  /*
+   method that sends the package to a receiver
+   calls choose_receiver method from ReceiverPreferences
+   then calls a receive_package method on a chosen receiver
+   empties the buffer
+   takes nothing
+   returns nothing
+   */
   IPackageReceiver* R = receiver_preferences.choose_receiver();
   R->receive_package(std::move(buffer.value()));
   buffer.reset();
 }
 
 void Storehouse::receive_package(Package&& package) {
+  /*
+   method that puts a given package into the storehouse queue
+   */
   packages->push(std::move(package));
 }
--- src/storage_types.cpp
+++ src/storage_types.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include "storage_types.hpp"
 
-void return_queue_type(QueueType t) {
+void return_queue_type(const QueueType t) {
+    /*function that outputs queue type name based on QueueType
+    takes const QueueType
+    returns nothing
+    */
     switch (t) {
         case QueueType::Fifo:
             std::cout << "fifo" << std::endl;
@@ -16,15 +20,23 @@ void return_queue_type(QueueType t) {
 }
 
 Package PackageQueue::pop() {
+    /*
+     Method that outputs a ceirtan object from the queue based on the queue type, and removes it from the queue
+     takes nothing
+     returns object of type Package
+     */
     Package p;
+    //implementation for QueueType Fifo
   if (qt == QueueType::Fifo) {
-      p = std::move(packages.front());
-      packages.pop_front();
-  }
-  else if (qt == QueueType::Lifo) {
       p = std::move(packages.back());
       packages.pop_back();
   }
+    //implementation for QueueType Lifo
+  else if (qt == QueueType::Lifo) {
+      p = std::move(packages.front());
+      packages.pop_front();
+  }
+    //error if invalid QueueType
   else {
     throw std::invalid_argument("Invalid QueueType");
   }
@@ -32,5 +44,10 @@ Package PackageQueue::pop() {
   }
 
 void PackageQueue::push( Package&& p) {
+    /*
+     Method that places an object into the queue
+     takes Package r-value
+     returns nothing
+     */
   packages.push_front(std::move(p));
 }
