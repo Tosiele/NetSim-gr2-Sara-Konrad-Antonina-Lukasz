b3efa222517ff48c540b457a07f913ad3767a665
KFerdek <kferdek@student.agh.edu.pl>
2026-01-25 15:25:11 +0100

    load_factory_structure complete
    Added LINK case

--- src/factory.cpp
+++ src/factory.cpp
@@ -15,19 +15,15 @@ ParsedLineData parse_line(const std::string &line) {
     char delimiter = ' ';
 
     std::getline(token_stream, token, delimiter);
+    std::map<std::string,ElementType> tags {
+        {"LOADING_RAMP",ElementType::RAMP},
+        {"WORKER",ElementType::WORKER},
+        {"STOREHOUSE",ElementType::STOREHOUSE},
+        {"LINK",ElementType::LINK}
+    };
 
-
-    if (token == "LOADING_RAMP") {
-        parsed_line.element_type = ElementType::RAMP;
-    }
-    else if (token == "WORKER") {
-        parsed_line.element_type = ElementType::WORKER;
-    }
-    else if (token == "STOREHOUSE") {
-        parsed_line.element_type = ElementType::STOREHOUSE;
-    }
-    else if (token == "LINK") {
-        parsed_line.element_type = ElementType::LINK;
+    if (tags.find(token) != tags.end()) {
+        parsed_line.element_type = tags.at(token);
     }
     else {
         throw std::invalid_argument("No matching tag");
@@ -53,6 +49,8 @@ Factory load_factory_structure(std::istream &is) {
     ElementID id;
     TimeOffset di;
     TimeOffset pd;
+    std::string src;
+    std::string dest;
 
     while (std::getline(is, line)) {
         if (line.empty() or line.substr(0,1) == ";") {
@@ -77,7 +75,69 @@ Factory load_factory_structure(std::istream &is) {
                 id = std::stoi(pdata.parameters.at("id"));
                 F.add_storehouse(Storehouse(id));
             case ElementType::LINK:
-                //TODO
+                std::map<std::string,ElementType> nodes{
+            {"ramp",ElementType::RAMP},
+            {"worker",ElementType::WORKER},
+            {"storehouse",ElementType::STOREHOUSE}
+                };
+
+                src = pdata.parameters.at("src");
+                dest = pdata.parameters.at("dest");
+                std::istringstream src_stream(src);
+                std::istringstream dest_stream(dest);
+                std::string src_node;
+                std::string dest_node;
+                std::string temp;
+                ElementID src_id;
+                ElementID dest_id;
+
+                std::getline(src_stream, src_node, '-');
+                std::getline(dest_stream, dest_node, '-');
+                std::getline(src_stream, temp, ' ');
+                src_id = std::stoi(temp);
+                std::getline(dest_stream, temp, ' ');
+                dest_id = std::stoi(temp);
+                ElementType src_type;
+                ElementType dest_type;
+                try {
+                    src_type = nodes.at(src_node);
+                    dest_type = nodes.at(dest_node);
+                }
+                catch (const std::out_of_range&) {
+                    std::cerr <<"No matching node type";
+                }
+                //probably should add try/catch
+                //when the id is not correct
+                //but dont know what error type is it
+                switch (src_type) {
+                    case ElementType::RAMP:
+                        auto R_src = F.find_ramp_by_id(src_id);
+                        switch (dest_type) {
+                            case ElementType::WORKER:
+                                auto W_dest = F.find_worker_by_id(dest_id);
+                                R_src->receiver_preferences.add_receiver(&(*W_dest));
+                            case ElementType::STOREHOUSE:
+                                auto S_dest = F.find_storehouse_by_id(dest_id);
+                                R_src->receiver_preferences.add_receiver(&(*S_dest));
+                            case default:
+                                throw std::invalid_argument("No matching receiver (RAMP can't be a receiver)");
+                        }
+                    case ElementType::WORKER:
+                        auto W_src = F.find_worker_by_id(src_id);
+                        switch (dest_type) {
+                            case ElementType::WORKER:
+                                auto W_dest = F.find_worker_by_id(dest_id);
+                                W_src->receiver_preferences.add_receiver(&(*W_dest));
+                            case ElementType::STOREHOUSE:
+                                auto S_dest = F.find_storehouse_by_id(dest_id);
+                                S_dest->receiver_preferences.add_receiver(&(*S_dest));
+                            case default:
+                                throw std::invalid_argument("No matching receiver (RAMP can't be a receiver)");
+                        }
+                    case default:
+                        throw std::invalid_argument("No matching source (STOREHOUSE can't be a sender)");
+                }
+
             case default:
                 throw std::invalid_argument("HOW???");
 
