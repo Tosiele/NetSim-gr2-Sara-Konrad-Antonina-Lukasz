59f7521964ce460739a87282e85375d0473bcf63
KFerdek <kferdek@student.agh.edu.pl>
2026-01-26 15:34:13 +0100

    IO complete
    Added documentation and tests. Still need to check correctness after adding the full Factory class

--- include/factory.hpp
+++ include/factory.hpp
@@ -12,30 +12,36 @@
 
 #include "nodes.hpp"
 
-enum class ElementType { //or regular enum?
+enum class ElementType {
+    //enumeration class that stores element types
+    //possible to pass in the configuration file
     RAMP,
     WORKER,
     STOREHOUSE,
     LINK
 };
 struct ParsedLineData {
+    //struct that stores the type of element
+    //along with all its properties as a map
     ElementType element_type;
     std::map<std::string, std::string> parameters;
     //only for tests
     bool operator==(const ParsedLineData &other) const {return (element_type == other.element_type && parameters == other.parameters);}
 };
 
-
+//function that divides the given line and stores the result as ParsedLineData struct
 ParsedLineData parse_line(const std::string &line);
 
+//function that makes a Factory based on the config file
 Factory load_factory_structure(std::istream &is);
 
 //helper functions for save_factory_structure
-std::string receiver_save_func(const ReceiverPreferences &rp);
+std::string receiver_save_func(auto &rp);
 void ramp_save_func(Ramp &ramp, std::ostream &os);
 void worker_save_func(Worker &worker, std::ostream &os);
 void storehouse_save_func(Storehouse &storehouse, std::ostream &os);
 
+//function that makes a factory structure report
 void save_factory_structure(Factory &factory, std::ostream &os);
 
 #endif //NETSIM_GR2_SARA_KONRAD_ANTONINA_HELPERS_HPP
